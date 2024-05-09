5b87f27902a4e3709e1e700806c01a075a1d324e
LUANNY DE BARROS RODRIGUES <53187812848@info.local>
2024-05-09 08:46:44 -0300

    feat: exercicio 1

---
+++ ex1.js
@@ -0,0 +1,15 @@
+let username = prompt ("digite um nome de usuário");
+
+let senha = prompt ("digite a senha");
+
+if (username == "admin" && senha == "senha123")
+ {console.log ("Login bem sucedido");
+}
+else {
+    console.log("VocÊ não tem permissão ao acesso!");
+}
+
+
+
+
+
---
+++ ex2.js
@@ -0,0 +1,7 @@
+let nota1 = parseInt(prompt ("Digite uma: "));
+
+let nota2 = parseInt(prompt("Digite outra nota: "));
+
+let media = (nota1+nota2) /2
+
+console.log (média)
