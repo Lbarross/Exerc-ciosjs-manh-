9d0e2f1dbf58026c29be9040ea10f898fee434ce
LUANNY DE BARROS RODRIGUES <53187812848@info.local>
2024-05-10 08:19:55 -0300

    feat: Exercicio 5

---
+++ ex5.js
@@ -0,0 +1,10 @@
+let ordem1 = parseInt (prompt ("Digite o primeiro número"))
+let ordem2 = parseInt (prompt ("Digite o segundo número"))
+let ordem3 = parseInt(prompt ("Digite o terceiro número"))
+
+if (ordem1< ordem2 && ordem2 <ordem3) {
+    console.log ("Ordem crescente!")
+}
+else {
+    console.log ("Não é uma ordem crescente!")
+}
