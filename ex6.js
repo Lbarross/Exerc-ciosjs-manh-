30ee3dedb5bbadabde05b7a3147133897a70ceed
LUANNY DE BARROS RODRIGUES <53187812848@info.local>
2024-05-10 08:25:46 -0300

    feat: Exercicio 6

---
+++ ex6.js
@@ -0,0 +1,8 @@
+let nascimento = prompt ("Digite seu  ano de nascimento")
+
+if (nascimento < 2006){
+    console.log ("Você é maior de idade!")
+}
+else {
+    console.log ("Você é menor de idade!")
+}
